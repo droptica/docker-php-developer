bf8b7c0229a6ddb5d22ed53b0aca1e43499c05ba
Daniel Kossakowski <daniel@krolnet.pl>
2019-09-16 10:32:25 +0200

    Remove python-MySQL package

--- build.sh
+++ build.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
+export MEMCACHED_VERSION=''
+export XDEBUG_VERSION=''
 export DRUSH_9_VER=9.6.2
 export DRUSH_8_VER=8.2.3
 export COMPOSER_VER=1.8.5
-export PHP_VERSION=5.6
-export MEMCACHED_VERSION="-2.2.0"
-export XDEBUG_VERSION="-2.5.5"
+export REDIS_VERSION="-4.3.0"
 
-envsubst '${DRUSH_9_VER},${DRUSH_8_VER},${COMPOSER_VER},${PHP_VERSION},${MEMCACHED_VERSION}${XDEBUG_VERSION}' < Dockerfile.tpl > Dockerfile
-docker build -t droptica/php-dev:${PHP_VERSION} .
-
-export MEMCACHED_VERSION=''
-export XDEBUG_VERSION=''
 for i in $(seq 1 3); do
     export PHP_VERSION=7.${i}
     envsubst '${DRUSH_9_VER},${DRUSH_8_VER},${COMPOSER_VER},${PHP_VERSION},${MEMCACHED_VERSION}${XDEBUG_VERSION}' < Dockerfile.tpl > Dockerfile
